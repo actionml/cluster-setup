a54fec0542dae1d109662028b22ca5f63e41e7c5
Feña Agar <ferliagno@gmail.com>
2014-10-06 19:59:19 +0200

    restructure

--- rails/resque.sh
+++
@@ -1,4 +0,0 @@
-rake resque:work QUEUE=cron --trace RAILS_ENV=production >> work.log &
-rake resque:scheduler --trace RAILS_ENV=production >> schedule.log &
-
-ps -fea | grep resque > resque_info.log
--- rojo/ZSH/.zshenv
+++
@@ -1,3 +0,0 @@
-
-PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-PATH="$PATH:/usr/local/mysql-5.5.28-osx10.6-x86_64/bin"

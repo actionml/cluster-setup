569e585aa0144db939659b2c8267ea873cfe554a
Feña Agar <ferliagno@gmail.com>
2015-09-07 20:28:23 +0200

    cleanup

--- scripts/git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "Feña Agar"
-git config --global user.email ferliagno@gmail.com
-git config --global color.ui true
-git config --global pull.rebase true
-git config --global push.default matching
